8b48abe94a853ee9f4a442e3f3dce3209756e942
SzabolcsMarton <maszabolcs@gmail.com>
2020-11-15 10:06:05 +0100

    add .env file

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 mongoose.connect(
-  "mongodb+srv://SzabolcsMarton:Szaba1980@hamburger.pnsbz.mongodb.net/Hamburger?retryWrites=true&w=majority",
+  process.env.DB_CONNECTION,
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => console.log("Db connected")
 );
@@ -25,6 +26,6 @@ app.use("/", pageRoute);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
