58267840f0e64d925c0a36c18803fa26558ed70d
SzabolcsMarton <maszabolcs@gmail.com>
2020-11-18 18:53:00 +0100

    fix

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 //body parser
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 //routes middleware
